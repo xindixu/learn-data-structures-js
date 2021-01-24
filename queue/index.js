1dc70077d6ea2f818fbd66ea75f1c98bf84982f3
Xindi Xu <xindixu@utexas.edu>
2021-01-24 14:13:30 -0600

    feat: implement queue

---
+++ queue/index.js
@@ -0,0 +1,51 @@
+class Queue {
+  #items = []
+
+  /** Add an item at the end of the queue
+   * @param  {any} item
+   */
+  enqueue(item) {
+    this.#items.push(item)
+  }
+
+  /** Remove the item at the head of the queue
+   * @returns {any} Item being removed
+   */
+  dequeue() {
+    return this.#items.shift()
+  }
+
+  /** Get the item at the head of the stack
+   * @returns {any} Item at the head
+   */
+  head() {
+    return this.#items[0]
+  }
+
+  /** Get the item at the end of the stack
+   * @returns {any} Item at the end
+   */
+  tail() {
+    return this.#items[this.#items.length - 1]
+  }
+
+  /** Check if the queue is empty
+   * @returns {Boolean} Is queue empty
+   */
+  isEmpty() {
+    return this.#items.length === 0
+  }
+
+  /** Get the size of the queue
+   * @returns {Number} Number of items in the queue
+   */
+  size() {
+    return this.#items.length
+  }
+
+  clear() {
+    this.#items = []
+  }
+}
+
+export default Queue
