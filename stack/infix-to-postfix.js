45164db29e75ab66957fd2f3ca68d2ce4e55665c
Xindi Xu <xindixu@utexas.edu>
2021-01-23 18:42:55 -0600

    exp: infix to postfix

---
+++ stack/infix-to-postfix.js
@@ -0,0 +1,59 @@
+import Stack from "./index"
+
+const infixToPostfix = (array) => {
+  const OPERATORS = { "+": 1, "-": 1, "*": 2, "/": 2 }
+  const op = new Stack()
+  const result = []
+
+  array.forEach((i) => {
+    if (i === "(") {
+      op.push(i)
+      return
+    }
+
+    if (i === ")") {
+      // Add everything until (
+      while (op.peek() !== "(") {
+        result.push(op.pop())
+      }
+      // Remove (
+      op.pop()
+      return
+    }
+
+    if (Object.keys(OPERATORS).includes(i)) {
+      /*
+
+      Compare:
+    
+      1 * 2 + 3 -> 1 2 * 3 +
+      1 + 2 * 3 -> 1 2 3 * + 
+
+      */
+
+      // Since we want to process operators with higher priority first, need to pop those out first
+      // Add all operators that has higher priority than current operator
+      while (
+        !op.isEmpty() &&
+        Object.keys(OPERATORS).includes(op.peek()) &&
+        OPERATORS[op.peek()] >= OPERATORS[i]
+      ) {
+        result.push(op.pop())
+      }
+      // Add current operator to stack
+      op.push(i)
+      return
+    }
+
+    // numbers
+    result.push(i)
+  })
+
+  while (!op.isEmpty()) {
+    result.push(op.pop())
+  }
+
+  return result
+}
+
+export default infixToPostfix
